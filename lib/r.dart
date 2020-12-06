92adf18ffe35ad257a609dbd7652572a9b8e5299
Dany <yasfdany@gmail.com>
2020-12-06 19:18:09 +0700

    Icon launcher

--- lib/r.dart
+++ lib/r.dart
@@ -1,4 +1,6 @@
 class R {
+  /// ![](http://127.0.0.1:9527/assets/images/ic_launcher.png)
+  static final String assetsImagesIcLauncher = 'assets/images/ic_launcher.png';
   /// ![](http://127.0.0.1:9527/assets/images/sleep_illustration.svg)
   static final String assetsImagesSleepIllustration = 'assets/images/sleep_illustration.svg';
 }
